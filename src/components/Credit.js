f2e6a537d12780c3a04cc9e3b9dd2eb00fe6f027
SaedEynte1 <codeey26@gmail.com>
2024-07-27 19:02:11 +0300

    commit

---
+++ src/components/Credit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Coded by
+      <a href="https://www.just.edu.so"
+        > JUST UNIVERSITY</a
+      >,
+    </p>
+  </div>
+  );
+}
